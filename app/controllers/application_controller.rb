4ac1f6e44d7392376dc9c4f73408a4498f77240d
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-01 13:17:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     post '/teams' do
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
-      @heros = params[:team][:members]
+      @hero = params[:team][:members][1]
       erb :team
     end
 
